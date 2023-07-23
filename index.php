8133bfc7b20f5fa3751baaafa0f7dd98a47065ce
sitry <sitry@users.noreply.github.com>
2023-07-23 17:01:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ $dbname = "mysql";
 $dbuser = "root";
 $dbpass = "n,BenAsVY6Vr";
 //Connect to database
-die("aaa");
-$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
 
+$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
+die("aaa");
 mysql_select_db($dbname, $conn) or die(mysql_error());
 
 $username = "root";
