68cc3bdf9aaca6971acc5c160440cbb5ef3293ea
sitry <sitry@users.noreply.github.com>
2023-07-23 23:07:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-session_start();
-$mysqli = new mysqli("localhost","root","s,mKl2;oEG_8");
-die("aaa");
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-
-	
+session_start();	
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "s,mKl2;oEG_8";
