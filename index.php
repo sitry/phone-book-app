3a54672255e24dcd4ac63ce8f8bcd4d915ec9d79
sitry <sitry@users.noreply.github.com>
2023-07-23 23:06:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,13 @@ if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
+
+	
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "s,mKl2;oEG_8";
+
+//Connect to database
+
+$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
 ?>
