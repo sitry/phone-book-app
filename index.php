85e5500b8b737a09be4b71aad0e1f98b99f8005b
sitry <sitry@users.noreply.github.com>
2023-07-23 18:06:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
 session_start();
-//Database Information
+$mysqli = new mysqli("localhost","my_user","my_password");
 
-$dbhost = "localhost";
-$dbname = "mysql";
-$dbuser = "root";
-$dbpass = "n,BenAsVY6Vr";
-//Connect to database
-
-$conn = mysql_connect($dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
-die("aaa");
-mysql_select_db($dbname, $conn) or die(mysql_error());
-
-$username = "root";
-$password = "password";
-
-  mysql_query("UPDATE mysql.user SET Password = PASSWORD('$password') WHERE User='$username'");
-  echo("Thank You. Your Password has been successfully changed.");
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
 ?>
