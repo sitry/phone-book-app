bf1edf3615bea996275e8404012981045ffb3099
sitry <sitry@users.noreply.github.com>
2023-08-01 23:13:45 +0300

    mysql installation

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-session_start();	
+/*session_start();	
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "s,mKl2;oEG_8";
 
 //Connect to database
 
-$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
+$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());*/
+phpinfo();
 ?>
