7520e2ea0c4f9af047eea37a2f10d6ad506f03ed
sitry <sitry@users.noreply.github.com>
2023-10-22 15:59:02 +0300

    Update index.php
    mysql connection success

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 session_start();	
-/* $dbhost = "localhost";
+$dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "1a2b3c4d";
-
-//Connect to database
-//$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
-phpinfo();*/
-  $conn = new mysqli("localhost", "root", "1a2b3c4d");
-  if ($conn->connect_error) echo "Connection failed: " . $conn->connect_error; 
-  else echo "Connected successfully";
-  phpinfo();
+$conn = new mysqli($dbhost, $dbuser, $dbpass);
+if ($conn->connect_error) echo "Connection failed: " . $conn->connect_error; 
+else echo "Connected successfully";
+phpinfo();
 ?>
