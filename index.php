aed839142327652f0aabb3fd40d9c79aa5f45a61
sitry <sitry@users.noreply.github.com>
2023-10-22 15:46:51 +0300

    Update index.php
    mysql connection still

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ $dbuser = "root";
 $dbpass = "1a2b3c4d";
 
 //Connect to database
-$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
+//$conn = mysql_connect ( $dbhost, $dbuser, $dbpass)or die("Could not connect: ".mysql_error());
 phpinfo();
 ?>
